7c70bdb86224edbeb8d016d1d94ca58a340977f7
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-09 20:48:13 -0400

    what

---
+++ Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace portfolio3.Models
+{
+    public class Project 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string gitUrl { get; set; }
+        public string liveUrl { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
